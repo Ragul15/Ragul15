8150654c9d9ef8939dc16bfe05cda61e4e029ee4
Ragul15 <53175597+Ragul15@users.noreply.github.com>
2019-07-25 11:28:30 +0500

    Update and rename equaltoz.txt to equaltoz.c

--- equaltoz.c
+++ equaltoz.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
-
 int main()
-
 {
-
    int i,j,N,n[20];
-
    scanf("%d",&N);
-
    for(i=0;i<N;i++)
-
    {
-
        scanf("%d",&n[i]);
-
    }
-
    for(i=0;i<N;i++)
-
    {
-
        for(j=i+1;j<N;j++)
-
        {
-
            if(n[i]+n[j]==0)
-
            {
-
                printf("%d %d\n",n[i],n[j]);
-
            }
-
        }
-
    }
-
-
     return 0;
-
 }
